69058de4db0e91efde5a953c835ef8f9272e2ad5
mlemons24 <mlemons24@gmail.com>
2021-12-26 14:26:26 -0600

    added data binding

--- app/src/main/java/com/example/authenticationapp/login/LoginFragment.kt
+++ app/src/main/java/com/example/authenticationapp/login/LoginFragment.kt
@@ -10,9 +10,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.example.authenticationapp.R
 import androidx.fragment.app.viewModels
 import com.example.authenticationapp.data.UserRepo
+import com.example.authenticationapp.databinding.LoginFragmentBinding
 import com.example.authenticationapp.forgot.ForgotPasswordFragment
 import com.example.authenticationapp.register.RegisterFragment
 import java.net.PasswordAuthentication
@@ -23,52 +25,40 @@ class LoginFragment : Fragment() {
         fun newInstance() = LoginFragment()
     }
 
-    private val viewModel: LoginViewModel by viewModels{
+    private val viewModel: LoginViewModel by viewModels {
         LoginVMFactory(UserRepo())
     }
-
-    private lateinit var emailET:EditText
-    private lateinit var passwordET:EditText
-    private lateinit var loginBTN:Button
-    private lateinit var registerBTN:Button
-    private lateinit var forgotpass:TextView
-
+    private lateinit var binding: LoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view =inflater.inflate(R.layout.login_fragment, container, false)
-        emailET= view?.findViewById(R.id.email_et)!!
-        passwordET= view?.findViewById(R.id.password_et)!!
-        loginBTN= view?.findViewById(R.id.login_btn)!!
-        registerBTN= view?.findViewById(R.id.registration_btn)!!
-        forgotpass= view?.findViewById(R.id.forgot_password)!!
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
 
-        loginBTN.setOnClickListener {
-            Log.d("click","clickedloginbtn")
+        binding.loginBtn.setOnClickListener {
+            Log.d("click", "clickedloginbtn")
         }
 
-        registerBTN.setOnClickListener {
-            Log.d("click","clickedregisterbtn")
+        binding.registrationBtn.setOnClickListener {
+            Log.d("click", "clickedregisterbtn")
             requireActivity().supportFragmentManager.beginTransaction()
-                .add(R.id.container,RegisterFragment.newInstance(),"register")
+                .add(R.id.container, RegisterFragment.newInstance(), "register")
                 .addToBackStack("register")
                 .commit()
 
 
         }
-        forgotpass.setOnClickListener {
-            Log.d("click","clickedforgotpasswordbtn")
+        binding.forgotPassword.setOnClickListener {
+            Log.d("click", "clickedforgotpasswordbtn")
             requireActivity().supportFragmentManager.beginTransaction()
-                .add(R.id.container,ForgotPasswordFragment.newInstance(),"forgotpass")
+                .add(R.id.container, ForgotPasswordFragment.newInstance(), "forgotpass")
                 .addToBackStack("forgotpass")
                 .commit()
-            }
+        }
 
-        return view
+        return binding.root
     }
-
 
 
 }
